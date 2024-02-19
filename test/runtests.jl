1675631cd8f4803b1ffd764b08b69b693ac0a1ee
hannahilea <hannahilea@users.noreply.github.com>
2024-02-19 13:24:49 -0500

    add aqua tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
 using Test
 using LearnLooper
+using Aqua
 
 @testset "LearnLooper" begin
+    @testset "Aqua" begin
+        Aqua.test_all(LearnLooper; ambiguities=false)
+    end
+
     @test isnothing(learn_demo())
 end
