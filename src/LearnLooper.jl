bd680849480e6da1c3376570e39af7cc7abc9b7d
hannahilea <hannahilea@users.noreply.github.com>
2024-02-21 21:19:10 -0500

    so it says

--- src/LearnLooper.jl
+++ src/LearnLooper.jl
@@ -1,75 +1,88 @@
 module LearnLooper
 
-export learn_demo, learn
+export learn_demo, learnloop
 
 const DEMO_ALPHABET = collect('a':'z')  #TODO-future: make this a loaded audio file
 
 #TODO-future: make this a blob that has more info, e.g. label + timestamp + metadata
-const DEMO_SEGMENTS = [1:5, 6:20, 21:26]  # assume non-overlapping and contiguous for the demo segments
+const DEMO_SPANS = [1:5, 6:20, 21:26]  # assume non-overlapping and contiguous for the demo spans
 
 """
-    play(input, segment; volume_scale=1) -> nothing
+    play(input; volume_scale=1) -> nothing
 
-Computer plays `segment` of `input`.
+Computer plays `span` of `input`. 
+Currently only supports Mac playback!
 """
-function play(input, segment; volume_scale=0.8, rate=200)
-    seg = string(input[segment])
+function play(input; volume_scale=0.8, rate=230)
+    seg = string(input)
     @debug "$(volume_scale == 0 ? "🎤Pausing" : "👂Playing"): $seg"
-    run(`say \[\[volm $(volume_scale)\]\] \[\[rate $rate\]\] $seg`)
+    Sys.isapple() && run(`say \[\[volm $(volume_scale)\]\] \[\[rate $rate\]\] $seg`)
     return nothing
 end
 
-function get_learnspan(i_segment, segments; iteration_mode)
+"""
+    pause(input) -> nothing
+
+Computer pauses for duration it would take to play `input`.
+"""
+pause(input) = play(input; volume_scale=0)
+
+function compute_learnspan(i_span, spans; iteration_mode)
     if iteration_mode == :cumulative
-        current_segments = segments[1:i_segment]
-        return length(current_segments) == 1 ? only(current_segments) :
-               vcat(current_segments)
+        current_spans = spans[1:i_span]
+        return length(current_spans) == 1 ? only(current_spans) :
+               collect(Iterators.flatten(current_spans)) #TODO: might be overkill
     end
     if iteration_mode != :sequential
         @warn "`iteration_mode=$mode` is unsupported; falling back to `:sequential` (options: `sequential`, `cumulative`)"
     end
-    return segments[i_segment]
+    return spans[i_span]
 end
+
+# If input is a single string, split it into words
+preprocess_input(input) = input
+preprocess_input(input::String) = string.(split(input, " "))
+preprocess_input(input::Number) = string(input)
 
 #####
 ##### Entrypoints
 #####
 
 """
-    pause(input, segment) -> nothing
-
-Computer pauses for duration `segment` of `input`.
-"""
-pause(input, segment) = play(input, segment; volume_scale=0)
-
-"""
-    learn(signal, segments; num_repetitions, iteration_mode) -> nothing
+    learnloop(input, spans; num_repetitions, iteration_mode) -> nothing
 
 #TODO-future: real docstring!
 - clarify difference between 0 and 1 repetitions; consider "num_playbacks" or similar
-- note that non-contiguous `segments` may result in a click in cumulative iteration mode
+- note that non-contiguous `spans` may result in a click in cumulative iteration mode
 """
-function learn(signal, segments; num_repetitions, iteration_mode, interrepeat_pause)
-    #TODO-future: safety-check the iteration_mode, num_repetitions, segment v signal length
+function learnloop(input, spans; num_repetitions, iteration_mode, interrepeat_pause=0)
+    #TODO-future: safety-check the iteration_mode, num_repetitions, span v input length
+    #TODO-future: if playing text, warn if not mac
     @info "Welcome to the LearnLooper: prepare to learn by looping!" num_repetitions iteration_mode interrepeat_pause
-    for i in eachindex(segments)
-        learn_span = get_learnspan(i, segments; iteration_mode)
+
+    input = preprocess_input(input)
+
+    for i in eachindex(spans)
+        span = compute_learnspan(i, spans; iteration_mode)
+        # subinput = view(input, span)  # Make type-based function??
+        subinput = input[span]
         for _ in 1:num_repetitions
-            play(signal, learn_span)
-            pause(signal, learn_span)
+            play(subinput)
+            pause(subinput)
             sleep(interrepeat_pause)
         end
 
-        # If no repetitions, just play the segment and move on---do not pause between segments!!
-        num_repetitions == 0 && play(signal, segment)
+        # If no repetitions, just play the span and move on---do not pause between spans!!
+        num_repetitions == 0 && play(subinput)
     end
     return nothing
+    #TODO: for testing, return vector of spans 
 end
 
 function learn_demo(; num_repetitions=2, iteration_mode=:sequential,
-                    segments=DEMO_SEGMENTS, interrepeat_pause=0.1)
-    return learn(DEMO_ALPHABET, segments; num_repetitions, iteration_mode,
-                 interrepeat_pause)
+                    spans=DEMO_SPANS, interrepeat_pause=0.1)
+    return learnloop(DEMO_ALPHABET, spans; num_repetitions, iteration_mode,
+                     interrepeat_pause)
 end
 
 end # module LearnLooper
--- test/runtests.jl
+++ test/runtests.jl
@@ -11,18 +11,26 @@ using Aqua
         isnothing(learn_demo())
     end
 
-    @testset "`get_learnspan`" begin
-        using LearnLooper: get_learnspan
+    @testset "`compute_learnspan`" begin
+        using LearnLooper: compute_learnspan
         contiguous_spans = [1:4, 5:8, 9:22]
-        @test get_learnspan(1, contiguous_spans; iteration_mode=:sequential) == 1:4
-        @test get_learnspan(2, contiguous_spans; iteration_mode=:sequential) == 5:8
+        @test compute_learnspan(1, contiguous_spans; iteration_mode=:sequential) == 1:4
+        @test compute_learnspan(2, contiguous_spans; iteration_mode=:sequential) == 5:8
 
-        @test get_learnspan(1, contiguous_spans; iteration_mode=:cumulative) == 1:4
-        @test get_learnspan(2, contiguous_spans; iteration_mode=:cumulative) == [1:4, 5:8]
+        @test compute_learnspan(1, contiguous_spans; iteration_mode=:cumulative) == 1:4
+        @test compute_learnspan(2, contiguous_spans; iteration_mode=:cumulative) ==
+              [1:4, 5:8]
 
         noncontiguous_spans = [1:4, 9:22]
-        @test get_learnspan(1, noncontiguous_spans; iteration_mode=:cumulative) == 1:4
-        @test get_learnspan(2, noncontiguous_spans; iteration_mode=:cumulative) ==
+        @test compute_learnspan(1, noncontiguous_spans; iteration_mode=:cumulative) == 1:4
+        @test compute_learnspan(2, noncontiguous_spans; iteration_mode=:cumulative) ==
               [1:4, 9:22]
+    end
+
+    @testset "`compute_learnspan`" begin
+        @test isnothing(learnloop(collect('a':'z'), [1:2, 3:4]; num_repetitions=2, iteration_mode=:cumulative))
+        @test isnothing(learnloop("A lone sentence is indexed by word", [1:2, 3:4]; num_repetitions=2, iteration_mode=:cumulative))
+        @test isnothing(learnloop(["A vector of phrases", "are indexed", "by phrase"], [1:2, 3:3]; num_repetitions=2, iteration_mode=:cumulative))
+        @test isnothing(learnloop(pi+0, [1:4, 5:8]; num_repetitions=2, iteration_mode=:cumulative))
     end
 end
