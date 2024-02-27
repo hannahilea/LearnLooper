5f4f8e2b2bb613ca9d365a3708a5c96de3913eb2
hannahilea <hannahilea@users.noreply.github.com>
2024-02-27 15:13:42 -0500

    fix docs formatting

--- LearnLooper.jl/docs/make.jl
+++ LearnLooper.jl/docs/make.jl
@@ -2,13 +2,13 @@ using LearnLooper
 using Documenter
 
 makedocs(; modules=[LearnLooper],
-    sitename="LearnLooper.jl",
-    authors="hannahilea",
-    pages=["API Documentation" => "index.md"],
-    strict=true)
+         sitename="LearnLooper.jl",
+         authors="hannahilea",
+         pages=["API Documentation" => "index.md"],
+         strict=true)
 
 deploydocs(; repo="github.com/hannahilea/ExampleMonorepo.jl.git",
-    push_preview=true,
-    dirname="LearnLooper",
-    tag_prefix="LearnLooper-",
-    devbranch="main")
+           push_preview=true,
+           dirname="LearnLooper",
+           tag_prefix="LearnLooper-",
+           devbranch="main")
