1ea44b9f056dd6b131bc92e25827d5d7830977aa
hannahilea <hannahilea@users.noreply.github.com>
2024-03-06 12:09:43 -0500

    wip no promises

--- LearnLooper.jl/src/gui.jl
+++ LearnLooper.jl/src/gui.jl
@@ -24,200 +24,83 @@ const ITERATION_MODE = Ref{Symbol}(:sequential)
 const SPANS = Ref{Any}(INPUT_SPANS[1])
 
 #####
-##### Signals
+##### Behavior
 #####
 
-# function on_mode_switched(self::Switch, app::Application)
-#     state = get_is_active(self)
-#     @debug "`mode_switch` is now: $state"
-#     # set_current_theme!(app, state ? THEME_DEFAULT_LIGHT : THEME_DEFAULT_DARK)
-#     return nothing
-# end
+const PLAYBACK_STATE = Ref{Union{Nothing,String}}(nothing)
 
-# function on_dryrun_switched(self::Switch)
-#     state = get_is_active(self)
-#     @debug "`dryrun_switch` is now: $state"
-#     DRY_RUN[] = state
-#     return nothing
-# end
+function on_play_pause_clicked(widgetptr, user_data)
+    @debug "play/pause clicked"
+    if isnothing(PLAYBACK_STATE)
+       # Start playing!! 
+       play_looper()
+    else 
+        pause_looper()
+    end
 
-# function on_play_toggled(self::ToggleButton)
-#     state = get_is_active(self)
-#     @debug "`play_button` is now: $state"
-#     set_child!(self, Label(state ? "Pause learning" : "Learn loop!"))
+    # # g_timeout_add can be used to periodically call a function from the main loop
+    # Gtk4.GLib.g_timeout_add(50) do  # create a function that will be called every 50 milliseconds
+    #     label.label = "counter: $(COUNTER[])"
+    #     return time() < stop_time   # return true to keep calling the function, false to stop
+    # end
 
-#     function mid_learn_callback(state)
-#         if state == "Play"
-#             # TODO: implement early exit by this, if pause button hit....?
-#             # println("State ", state)
-#         end
-#         @debug string("State ", state)
-#         return nothing
-#     end
+    Threads.@spawn begin
+        tid = Threads.threadid()
+        tp = Threads.threadpool()
 
-#     if state
-#         out = learn_loop(INPUT_PATH[], SPANS[]; num_repetitions=NUM_REPETITIONS[],
-#                          iteration_mode=ITERATION_MODE[], dryrun=DRY_RUN[]) #,
-#         #  state_callback=mid_learn_callback)
-#         println(out)
-#         set_is_active!(self, false)
-#     end
-#     return nothing
-# end
+        @debug "Playing" tid tp
+        out = learn_loop(collect('a':'z'), [1:2, 3:4]; num_repetitions=1,
+                         iteration_mode=:sequential, dryrun=DRY_RUN_MODE[])
+
+        # Interacting with GTK from a thread other than the main thread is
+        # generally not allowed, so we register an idle callback instead.
+        Gtk4.GLib.g_idle_add() do
+            print("Thread $tid in the $tp threadpool: ", out)
+            return false
+        end
+    end
+    return nothing
+end
+
+function on_dry_run_clicked(_, data)
+    dryrun_mode = DRY_RUN_MODE[]
+    println("Dry run mode ", dryrun_mode ? "disabled" : "enabled", "!")
+    DRY_RUN_MODE[] = !dryrun_mode
+    data.button.label = dryrun_mode ? "Enable dry run mode" : "Disable dry run mode"
+    return nothing
+end
 
 #####
 ##### GUI
 #####
 
-# function construct_gui!(app::Application, window)
-#     header_bar = get_header_bar(window)
-
-#     # Update window title
-#     set_title!(window, "Learn Looper")
-
-#     # Add play/pause button 
-#     play_button = let
-#         button = ToggleButton()
-#         set_child!(button, Label("Learn loop!")) #TODO: add emoji! (and in function)
-#         connect_signal_toggled!(on_play_toggled, button)
-#         button
-#     end
-
-#     # Add dark/light mode toggle
-#     dark_mode_button = let
-#         switch = Switch()
-#         set_is_active!(switch, DEFAULT_DARK_LIGHT_THEME == THEME_DEFAULT_LIGHT)
-#         switch_box = vbox(switch, Label("Dark/light"))
-#         set_horizontal_alignment!(switch, ALIGNMENT_CENTER)
-#         set_margin!(switch, 10)
-#         connect_signal_switched!(on_mode_switched, switch, app)
-#         switch_box
-#     end
-
-#     # Add dryrun switch
-#     dry_run_button = let
-#         switch = Switch()
-#         set_is_active!(switch, false)
-#         switch_box = vbox(switch, Label("Dry run enabled (debug)"))
-#         set_horizontal_alignment!(switch, ALIGNMENT_CENTER)
-#         set_margin!(switch, 10)
-#         connect_signal_switched!(on_dryrun_switched, switch)
-#         switch_box
-#     end
-
-#     # Place them!
-#     box = hbox(dry_run_button, dark_mode_button, play_button)
-#     for widget in [dry_run_button, dark_mode_button, play_button]
-#         set_horizontal_alignment!(widget, ALIGNMENT_CENTER)
-#         set_margin!(widget, 10)
-#         # push_back!(box, widget)
-#     end
-#     set_margin!(box, 75)
-
-#     # # Position the widgets
-#     # set_start_child!(box, play_button)
-#     # set_end_child!(box, dark_mode_button)
-#     # set_margin!(box, 75)
-
-#     set_child!(window, box)
-
-#     return nothing
-# end
-
-# TODO-future: refactor for cleaner UI
-# function create_app(app::Application)
-#     window = Window(app)
-#     construct_gui!(app, window)
-#     set_current_theme!(app, DEFAULT_DARK_LIGHT_THEME)
-#     present!(window)
-#     return nothing
-# end
-
 const COUNTER = Ref(0)
 
-function set_up_gui()     
+DRY_RUN_MODE = Ref(false)
+
+function set_up_gui()
     box = GtkBox(:v; name="content_box")
-        
+
     # Add buttons to UI
     dry_run_button = GtkButton("Dry run \n 🚧")
     push!(box, dry_run_button)
+    Gtk4.on_clicked(on_dry_run_clicked, dry_run_button, (button=dry_run_button,))
 
     play_pause_button = GtkButton("Learn loop! \n ⏯️")
     push!(box, play_pause_button)
-    # TODO: connect to on_play_pause_click
-    
+    Gtk4.on_clicked(on_play_pause_clicked, play_pause_button, ("foo", "bar"))
+
     # Finally, set up the main app window itself!
     win = GtkWindow("LearnLooper!", 420, 200, false) # Last arg is "resizable" TODO-future: add Gtk function for setting via kwargs
-    push!(win, box_o_stuff)
+    push!(win, box)
     return win
-end
-
-function on_play_pause_click(widget)
-    stop_time = time() + 3
-
-    # g_timeout_add can be used to periodically call a function from the main loop
-    Gtk4.GLib.g_timeout_add(50) do  # create a function that will be called every 50 milliseconds
-        label.label = "counter: $(COUNTER[])"
-        return time() < stop_time   # return true to keep calling the function, false to stop
-    end
-
-    Threads.@spawn begin
-        # Do work
-
-        COUNTER[] = 0
-        while time() < stop_time
-            COUNTER[] += 1
-        end
-
-        tid = Threads.threadid()
-        tp = Threads.threadpool()
-
-        # Interacting with GTK from a thread other than the main thread is
-        # generally not allowed, so we register an idle callback instead.
-        Gtk4.GLib.g_idle_add() do
-            
-            ent.text = "I counted to $(COUNTER[]) in thread $tid in the $tp threadpool!"
-            return false
-        end
-    end
-end
-
-# TODO-remove: copied from multithread example
-function on_button_click(widget)
-    start(sp)
-    stop_time = time() + 3
-
-    # g_timeout_add can be used to periodically call a function from the main loop
-    Gtk4.GLib.g_timeout_add(50) do  # create a function that will be called every 50 milliseconds
-        label.label = "counter: $(COUNTER[])"
-        return time() < stop_time   # return true to keep calling the function, false to stop
-    end
-
-    Threads.@spawn begin
-        # Do work
-
-        COUNTER[] = 0
-        while time() < stop_time
-            COUNTER[] += 1
-        end
-
-        tid = Threads.threadid()
-        tp = Threads.threadpool()
-
-        # Interacting with GTK from a thread other than the main thread is
-        # generally not allowed, so we register an idle callback instead.
-        Gtk4.GLib.g_idle_add() do
-            stop(sp)
-            ent.text = "I counted to $(COUNTER[]) in thread $tid in the $tp threadpool!"
-            return false
-        end
-    end
 end
 
 function launch_gui()
     if Threads.nthreads() == 1 && Threads.nthreads(:interactive) < 1
-        @warn("This example is intended to be run with multiple threads enabled, e.g. `julia --threads=2`")
+        @warn("This application is intended to be run with multiple threads enabled, e.g. `julia --threads=2`")
     end
+    run(`say "launch gui"`)
 
     return set_up_gui()
 end
